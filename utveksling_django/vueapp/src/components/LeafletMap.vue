a4b1a1a2dbb7dcf30a79f4e4d15371ea04682f96
Erikseeeeen <eivindseriksen@gmail.com>
2023-07-27 16:44:38 +0200

    Add popups

--- utveksling_django/vueapp/src/components/LeafletMap.vue
+++ utveksling_django/vueapp/src/components/LeafletMap.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="map-container">
-    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-map ref="map" v-model:zoom="zoom" :center="mapCenter">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
-      <!-- Loop through the universities and place markers on the map -->
-      <!-- <l-marker v-for="(coord, index) in universities" :key="index" :lat-lng="[coord.lat, coord.lng]"></l-marker> -->
-      <!-- Radius should be the square root of university.number_of_students -->
       <l-circle-marker
         v-for="(university, index) in universities"
         :key="index"
         :lat-lng="[university.lat, university.lng]"
-        :radius="Math.sqrt(university.number_of_students) * 5"
+        :radius="this.zoom * Math.sqrt(university.number_of_students) * 3"
         :color="university.number_of_students ? 'red' : 'transparent'"
-      ></l-circle-marker>
+      >
+        <l-popup> 
+          <i>{{ university.name }}</i>
+          
+          <br />
+          {{university.number_of_students}}
+          <span v-if="university.number_of_students > 1">
+            rapporter fra {{ this.input_program }}
+          </span>
+          <span v-else>
+            rapport fra {{ this.input_program }}
+          </span>
+
+        </l-popup>
+      </l-circle-marker>
     </l-map>
   </div>
 </template>
 
-
-
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LCircleMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LCircleMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 import axios from "axios";
 
 export default {
@@ -32,59 +41,44 @@ export default {
     LMap,
     LTileLayer,
     LCircleMarker,
+    LPopup,
   },
   data() {
     return {
       zoom: 2,
-      mapCenter: [0, 0], // Default center universities
-      universities: [], // Array to store the fetched universities
+      mapCenter: [47.41322, -1.219482],
+      universities: [],
       input_program: "datateknologi",
     };
   },
   async mounted() {
-    // Fetch the list of universities from Django backend
     try {
       this.fetchInputProgram();
       const response = await axios.get("/api/university");
-
-      // for each university in response.data, print university.lng
-      // response.data.forEach((university) => {
-
-      //   // console.log(university.programs_serialized.replace(/'/g, '"').replace(/,}/g, "}"));
-      //   console.log(JSON.parse(university.programs_serialized.replace(/'/g, '"').replace(/,}/g, "}"))["datateknologi"])
-        
-
-      //   // if(university.programs_serialized.replace(/'/g, '"')[255] == "'")
-      //   // {
-      //   //   // console.log(university.programs_serialized);
-      //   // }
-      // });
-      
-
       this.universities = response.data.map((university) => {
         return {
           lat: parseFloat(university.lat),
           lng: parseFloat(university.lng),
-          // university contains a dictionary 'dict' as a JSON string. Set the property number to the value of 'my number' in this serialized dict
-          number_of_students: JSON.parse(university.programs_serialized.replace(/'/g, '"').replace(/,}/g, "}"))[this.input_program],
-
+          number_of_students: JSON.parse(
+            university.programs_serialized.replace(/'/g, '"').replace(/,}/g, "}")
+          )[this.input_program],
+          name: university.name, // Include the university name
         };
       });
-
     } catch (error) {
       console.error("Error fetching universities:", error);
     }
   },
   methods: {
     fetchInputProgram() {
-      // Fetch program names from Django backend using API endpoint
-      axios.get('/api/input_program')
-        .then(response => {
-          this.input_program = response.data['text'];
+      axios
+        .get("/api/input_program")
+        .then((response) => {
+          this.input_program = response.data["text"];
           console.log(response);
         })
-        .catch(error => {
-          console.error('Error fetching input_program:', error);
+        .catch((error) => {
+          console.error("Error fetching input_program:", error);
         });
     },
   },
@@ -93,58 +87,6 @@ export default {
 
 
 
-<!-- <script>
-import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-  },
-  data() {
-    return {
-      zoom: 2,
-    };
-  },
-};
-</script> -->
-
-<!-- <script>
-import LeafletMap from './components/LeafletMap.vue'
-import axios from 'axios';
-
-export default {
-  name: 'App',
-  components: {
-    LeafletMap
-  },
-  data() {
-    return {
-      universities: []
-    };
-  },
-  async mounted() {
-    try {
-      // Print "hello world"
-      console.log('Hello world!');
-
-      const response = await axios.get('/api/university'); // Replace with your API endpoint URL
-      this.universities = response.data;
-    } catch (error) {
-      console.error('Error fetching universities:', error);
-    }
-  },
-}
-</script> -->
-
-
-
-
-
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
